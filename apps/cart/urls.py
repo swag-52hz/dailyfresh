d9f411e6265ff4634f62f51a9354944db6d9a6d0
swag <1254846545@qq.com>
2020-02-24 11:49:09 +0800

    用户购物车模块功能完善

--- apps/cart/urls.py
+++ apps/cart/urls.py
@@ -5,4 +5,7 @@ from . import views
 app_name = 'cart'
 urlpatterns = [
     path('add/', views.AddCartView.as_view(), name='add_cart'),
+    path('', views.CartInfoView.as_view(), name='info'),
+    path('update/', views.UpdateCartView.as_view(), name='update'),
+    path('delete/', views.DeleteCartView.as_view(), name='delete')
 ]
--- apps/cart/views.py
+++ apps/cart/views.py
@@ -37,3 +37,82 @@ class AddCartView(View):
         return JsonResponse({'res':5, 'message':'购物车添加成功！', 'cart_count':cart_count})
 
 
+class CartInfoView(View):
+    def get(self, request):
+        user = request.user
+        cart_key = 'cart_%d' % user.id
+        conn = get_redis_connection('default')
+        cart_dict = conn.hgetall(cart_key)
+        skus = []
+        total_count = 0
+        total_price = 0
+        for sku_id, count in cart_dict.items():
+            sku = GoodsSKU.objects.get(id=sku_id)
+            count = int(count)
+            amount = sku.price * count
+            sku.amount = amount
+            sku.count = count
+            skus.append(sku)
+            total_count += count
+            total_price += amount
+        return render(request, 'cart/cart.html', locals())
+
+
+class UpdateCartView(View):
+    """更新购物车记录视图"""
+    def post(self, request):
+        user = request.user
+        if not user.is_authenticated:
+            return JsonResponse({'res': 0, 'errmsg': '请先登录后再添加购物车！'})
+        sku_id = request.POST.get('sku_id', '')
+        count = request.POST.get('count', '')
+        if not all([sku_id, count]):
+            return JsonResponse({'res': 1, 'errmsg': '数据不全！'})
+        try:
+            count = int(count)
+        except Exception as e:
+            return JsonResponse({'res': 2, 'errmsg': '商品数目错误！'})
+        try:
+            sku = GoodsSKU.objects.get(id=sku_id)
+        except GoodsSKU.DoesNotExist as e:
+            return JsonResponse({'res': 3, 'errmsg': '商品不存在！'})
+        # 进行数据库操作
+        conn = get_redis_connection('default')
+        cart_key = 'cart_%d' % user.id
+        if count > sku.stock:
+            return JsonResponse({'res':4, 'errmsg':'商品库存不足！'})
+        conn.hset(cart_key, sku_id, count)
+        # 计算用户购物车中商品总件数
+        vals = conn.hvals(cart_key)
+        total_count = 0
+        for val in vals:
+            total_count += int(val)
+        return JsonResponse({'res':5, 'total_count':total_count, 'message':'购物车更新成功！'})
+
+
+class DeleteCartView(View):
+    """删除购物车记录视图"""
+    def post(self, request):
+        user = request.user
+        if not user.is_authenticated:
+            return JsonResponse({'res':0, 'errmsg':'用户未登录！'})
+
+        sku_id = request.POST.get('sku_id')
+        if not sku_id:
+            return JsonResponse({'res':1, 'errmsg':'无效的sku_id'})
+        try:
+            sku = GoodsSKU.objects.get(id=sku_id)
+        except GoodsSKU.DoesNotExist as e:
+            return JsonResponse({'res':2, 'errmsg':'商品不存在！'})
+        conn = get_redis_connection('default')
+        cart_key = 'cart_%d' % user.id
+        conn.hdel(cart_key, sku_id)
+        # 计算用户购物车中商品总件数
+        vals = conn.hvals(cart_key)
+        total_count = 0
+        for val in vals:
+            total_count += int(val)
+        return JsonResponse({'res':3, 'total_count':total_count, 'message':'购物车记录删除成功！'})
+
+
+
